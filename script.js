d7e366b5a8bbecd48ad0cd38eaa30351d279e9cb
Cristian Amorim <camorim1100@gmail.com>
2025-12-03 16:46:19 -0300

    Adicionando efeito de expansão ao clicar nas imagens dos produtos

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ function openLinkInstagram(url) {
 function openLinkWhatsapp(url) {
     window.location.href = url;
 }
+
+function expandImage(img) {
+    const imagens = document.querySelectorAll('.produtos img');
+
+    imagens.forEach(i => {
+        if (i !== img) {
+            i.classList.remove('img-expandida');
+        }
+    });
+
+    img.classList.toggle('img-expandida');
+}
